a41f751171fa4af05d0face0faac9e46ccae3ee5
imaisnaini <imaisnaini@gmail.com>
2018-11-02 03:44:46 +0700

    Linking Drawer Menu
    [+] HelpActivity.java
    [+] InfoActivity.java
    [+] OrderActivity.java
    [+] ProfileActivity.java
    [+] activity_help.xml
    [+] activity_info.xml
    [+] activtiy_order.xml
    [+] activity_profile.xml
    [+] ic_help.png
    
    [update] MainActivity.java : add Navigation Drawer Listener
    [update] navigation_drawer_items.xml : change item id

--- app/src/main/java/com/rplsukses/ezprint/ui/activity/HelpActivity.java
+++ app/src/main/java/com/rplsukses/ezprint/ui/activity/HelpActivity.java
@@ -2,50 +2,34 @@ package com.rplsukses.ezprint.ui.activity;
 
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.rplsukses.ezprint.R;
-import com.rplsukses.ezprint.ui.adapter.TabAdapter;
-import com.rplsukses.ezprint.ui.fragment.KategoriFragment;
-import com.rplsukses.ezprint.ui.fragment.LocationFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
-    private TabAdapter mAdapter;
+public class HelpActivity extends AppCompatActivity {
 
-    @BindView(R.id.activity_main_viewPager)
-    ViewPager mViewPager;
-    @BindView(R.id.activity_main_tabLayout)
-    TabLayout mTabLayout;
-    @BindView(R.id.activity_main_toolbar)
-    Toolbar mToolbar;
-    @BindView(R.id.drawerLayout)
-    DrawerLayout mDrawerLayout;
-    @BindView(R.id.navigationView) NavigationView mNavView;
-
+    @BindView(R.id.drawerLayout)DrawerLayout mDrawerLayout;
+    @BindView(R.id.navigationView)NavigationView mNavView;
+    @BindView(R.id.activity_help_toolbar)Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_help);
         ButterKnife.bind(this);
         init();
     }
 
-    private void init() {
+    private void init(){
         setSupportActionBar(mToolbar);
-        mAdapter = new TabAdapter(getSupportFragmentManager());
 
         // Set Drawerlayout switch indicator that the icon leftmost Toolbar
         ActionBarDrawerToggle mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.lbl_open, R.string.lbl_close);
@@ -95,35 +79,5 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-        mAdapter.addFragment(new LocationFragment(), getString(R.string.tab_location));
-        mAdapter.addFragment(new KategoriFragment(), getString(R.string.tab_category));
-        mViewPager.setAdapter(mAdapter);
-        mTabLayout.setupWithViewPager(mViewPager);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
-
-        MenuItem mSearch = menu.findItem(R.id.action_search);
-
-        SearchView mSearchView = (SearchView) mSearch.getActionView();
-        mSearchView.setQueryHint("Search");
-
-        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //mAdapter.getFilter().filter(newText);
-                return true;
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/rplsukses/ezprint/ui/activity/OrderActivity.java
+++ app/src/main/java/com/rplsukses/ezprint/ui/activity/OrderActivity.java
@@ -8,9 +8,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -22,28 +20,24 @@ import com.rplsukses.ezprint.ui.fragment.LocationFragment;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity {
     private TabAdapter mAdapter;
 
-    @BindView(R.id.activity_main_viewPager)
-    ViewPager mViewPager;
-    @BindView(R.id.activity_main_tabLayout)
-    TabLayout mTabLayout;
-    @BindView(R.id.activity_main_toolbar)
-    Toolbar mToolbar;
-    @BindView(R.id.drawerLayout)
-    DrawerLayout mDrawerLayout;
-    @BindView(R.id.navigationView) NavigationView mNavView;
+    @BindView(R.id.drawerLayout)DrawerLayout mDrawerLayout;
+    @BindView(R.id.navigationView)NavigationView mNavView;
+    @BindView(R.id.activity_order_tabLayout)TabLayout mTabLayout;
+    @BindView(R.id.activity_order_toolbar)Toolbar mToolbar;
+    @BindView(R.id.activity_order_viewPager)ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_order);
         ButterKnife.bind(this);
         init();
     }
 
-    private void init() {
+    private void init(){
         setSupportActionBar(mToolbar);
         mAdapter = new TabAdapter(getSupportFragmentManager());
 
@@ -96,34 +90,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mAdapter.addFragment(new LocationFragment(), getString(R.string.tab_location));
-        mAdapter.addFragment(new KategoriFragment(), getString(R.string.tab_category));
+        //mAdapter.addFragment(new LocationFragment(), getString(R.string.tab_location));
+        //mAdapter.addFragment(new KategoriFragment(), getString(R.string.tab_category));
         mViewPager.setAdapter(mAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
-
-        MenuItem mSearch = menu.findItem(R.id.action_search);
-
-        SearchView mSearchView = (SearchView) mSearch.getActionView();
-        mSearchView.setQueryHint("Search");
-
-        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //mAdapter.getFilter().filter(newText);
-                return true;
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
     }
 }
